0df56465b0a688c3c1dd0c7baf3ee1c78af8efb9
Bjorn <vandoorn.bjorn@gmail.com>
2021-12-04 17:08:57 +0100

    Missing why the solution I've created is off by one

--- day1/main.py
+++ day1/main.py
@@ -1,8 +1,14 @@
 
-depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+file = open('./input.txt')
+content = file.read()
+depths = content.splitlines()
 
+print(len(depths))
+
+# depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
 def changed_depth(depths):
+    results = 0
     changes = len(depths)
     for i in range(0, changes):
         current_value = depths[i]
@@ -11,8 +17,11 @@ def changed_depth(depths):
             print(str(current_value) + " (N/A - no previous measurement)")
         elif current_value > previous_value:
             print(str(current_value) + " (Increased)")
+            results = results + 1
         else:
-            print(str(current_value) + " (decreased)")
+            print(str(current_value) + " (Decreased)")
+    return results
 
 
-changed_depth(depths)
+solution = changed_depth(depths)
+print(solution)
