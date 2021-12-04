6dd9edf485fc31fc0e233ea20204382bea8062cc
Bjorn <vandoorn.bjorn@gmail.com>
2021-12-04 15:30:06 +0100

    Update to use elif for cleaner code

--- day1/main.py
+++ day1/main.py
@@ -6,16 +6,13 @@ def changed_depth(depths):
     changes = len(depths)
     for i in range(0, changes):
         current_value = depths[i]
+        previous_value = depths[i - 1]
         if i == 0:
             print(str(current_value) + " (N/A - no previous measurement)")
-            continue
-        previous_value = depths[i - 1]
-        if current_value > previous_value:
+        elif current_value > previous_value:
             print(str(current_value) + " (Increased)")
-            continue
         else:
             print(str(current_value) + " (decreased)")
-            continue
 
 
 changed_depth(depths)
